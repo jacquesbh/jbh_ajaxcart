80e437a4c0a7a266922c55f1fae8fd83cf59e47f
Jacques Bodin-Hullin <jacques@bodin-hullin.net>
2013-01-25 00:18:04 +0100

    Updated install script

--- install.sh
+++ install.sh
@@ -40,6 +40,7 @@ case $1 in
         ln -s `pwd`/app/code/$pool/${namespace}/${module} $workspace/app/code/$pool/${namespace}/${module}
         ln -s `pwd`/app/etc/modules/${namespace}_${module}.xml $workspace/app/etc/modules/${namespace}_${module}.xml
         ln -s `pwd`/app/design/frontend/base/default/layout/${namespaceLower}_${moduleLower}.xml $workspace/app/design/frontend/base/default/layout/${namespaceLower}_${moduleLower}.xml
+        ln -s `pwd`/app/design/frontend/base/default/template/${namespaceLower}_${moduleLower} $workspace/app/design/frontend/base/default/template/${namespaceLower}_${moduleLower}
         ln -s `pwd`/js/${namespaceLower}_${moduleLower} $workspace/js/${namespaceLower}_${moduleLower}
     ;;
     uninstall)
@@ -47,6 +48,7 @@ case $1 in
         rm $workspace/app/code/$pool/${namespace}/${module} 2> /dev/null
         rm $workspace/app/etc/modules/${namespace}_${module}.xml 2> /dev/null
         rm $workspace/app/design/frontend/base/default/layout/${namespaceLower}_${moduleLower}.xml 2> /dev/null
+        rm $workspace/app/design/frontend/base/default/template/${namespaceLower}_${moduleLower} 2> /dev/null
         rm $workspace/js/${namespaceLower}_${moduleLower} 2> /dev/null
     ;;
     *)
